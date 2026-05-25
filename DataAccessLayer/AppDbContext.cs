9e827e51052d3411521b16120813e860348cbb43
tuanvy <maituanvyst@gmail.com>
2026-05-25 16:32:38 +0700

    set up

---
+++ DataAccessLayer/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pgvector.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class AppDbContext : DbContext
+    {
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Kích hoạt extension pgvector trong PostgreSQL
+            modelBuilder.HasPostgresExtension("vector");
+        }
+    }
+}
